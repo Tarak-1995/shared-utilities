5794c974c8d4c3e11490b2c3d79b8b79882a65bd
Ramesh Kundula <Ramesh.Kundula@primeroedge.com>
2021-02-21 00:17:07 +0000

    Updated version.ps1

--- PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/version.ps1
+++ PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/version.ps1
@@ -1,3 +1,4 @@
 ﻿$versionjson = (Get-Content "PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/sharedutilities/version.json" -Raw) | ConvertFrom-Json
 $env:Version=$versionjson.psobject.properties.value
 #Write-Host "##vso[task.setvariable variable=Version;]$version"
+Write-Host "Using version: ($env:Version)"
