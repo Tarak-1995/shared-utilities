52a476740d180b2e6919b1a2e0a4952c02b405a5
Ramesh Kundula <Ramesh.Kundula@primeroedge.com>
2021-02-22 17:34:05 +0000

    Updated version.ps1

--- PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/version.ps1
+++ PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/version.ps1
@@ -1,5 +1,5 @@
 ﻿$versionjson = (Get-Content "PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/devops/deployment/sharedutilities/version.json" -Raw) | ConvertFrom-Json
 $versionfromjson=$versionjson.psobject.properties.value
-Write-Host "##vso[task.setvariable variable=Version;]$versionfromjson" 
+Write-Host "##vso[task.setvariable variable=imageVersion;]$versionfromjson" 
 Write-Host "##vso[task.setvariable variable=helmChartVersion;]$versionfromjson"
-#Write-Host "Using version: ($env:Version)"
+#Write-Host "Using version: ($env:imageVersion)"
