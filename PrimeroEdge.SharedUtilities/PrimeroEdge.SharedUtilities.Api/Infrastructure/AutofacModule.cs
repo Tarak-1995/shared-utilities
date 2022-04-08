2896080f21c7f3ba5e345fc55dbbd6ea09dbceca
Hussain Yatam <Hussain.Yatam@primeroedge.com>
2022-04-08 12:27:05 -0500

    Removed mongo references

--- PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/Infrastructure/AutofacModule.cs
+++ PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Api/Infrastructure/AutofacModule.cs
@@ -7,16 +7,7 @@
  */
 
 using Autofac;
-using Cybersoft.Platform.Configuration;
-using Cybersoft.Platform.Data.MongDb;
-using Cybersoft.Platform.Utilities;
 using PrimeroEdge.SharedUtilities.Components;
-using System;
-using System.Threading.Tasks;
-using Cybersoft.Platform.Contracts;
-using Cybersoft.Platform.KeyVault;
-using Cybersoft.Platform.Logging;
-using Cybersoft.Platform.Utilities.ResponseModels;
 using Microsoft.Extensions.Configuration;
 using AutoMapper;
 using AutoMapper.Data;
@@ -44,39 +35,17 @@ namespace PrimeroEdge.SharedUtilities.Api
 
             builder.Register<IMapper>(c => mapper).InstancePerLifetimeScope();
 
-            //Configuration settings
-            builder.AddConfiguration();
-            builder.ConfigureKeyVault();
-
             //Audit settings
             builder.RegisterType<AuditManager>().As<IAuditManager>().SingleInstance();
             builder.RegisterType<AuditRepository>().As<IAuditRepository>().SingleInstance();
 
             // Connection strings
-            builder.RegisterSettings<ConnectionStrings>(ConfigKeys.ConnectionStrings);
             builder.Register<ISqlDbManager>((c) =>
             {
-                var connectionStrings = c.Resolve<Lazy<Task<ConnectionStrings>>>().Value.Result;
                 var iMapper = c.Resolve<IMapper>();
-                var connString = c.DecryptKeyVaultString(CryptoManager.CONNECTION_STRING_KEY, connectionStrings.Connections[ConnectionType.ADMINISTRATION.ToString()]);
+                var connString = c.Resolve<IConfiguration>().GetValue<string>("ConnectionStrings:ADMINISTRATION");
                 return new SqlDbManager(connString, iMapper);
             }).SingleInstance();
-
-            builder.RegisterSettings<ErrorMessageSettings>(ConfigKeys.ErrorMessageSettings);
-            builder.Register<IMongoDbManager<MessageData>>(c =>
-            {
-                var settings = c.Resolve<Lazy<Task<ErrorMessageSettings>>>().Value.Result;
-                settings.MongoDbSettings.ConnectionString = c.DecryptKeyVaultString(CryptoManager.CONNECTION_STRING_KEY, settings.MongoDbSettings.ConnectionString);
-                return new MongoDbManager<MessageData>(settings.MongoDbSettings);
-            }).SingleInstance();
-
-
-            builder.Register<ICybersoftLogger>(c =>
-            {
-                var settings = c.Resolve<IConfiguration>().GetSection("LogSettings").Get<LogSettings>();
-                return LoggerFactory.GetLogger(settings);
-            });
-
         }
     }
 }
