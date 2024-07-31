7cd92f01f839e529db4be08d8b5e058c5a49a0e7
Henry Mataji <Henry.Mataji@primeroedge.com>
2024-07-31 15:10:50 -0500

    auditlog

---
+++ PrimeroEdge.SharedUtilities/PrimeroEdge.SharedUtilities.Components/Models/AuditLogEntityEx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PrimeroEdge.SharedUtilities.Components.Models;
+
+public class AuditLogEntityEx : ITableEntity
+{
+    public AuditLogEntityEx()
+    {
+    }
+    /// <summary>
+    /// Gets or sets verificationSampleId
+    /// </summary>
+    public Guid AuditId { get; set; }
+
+    /// <summary>
+    /// Get or set RegionId
+    /// </summary>
+    public int RegionId { get; set; }
+
+    /// <summary>
+    /// Get or set ModuleId
+    /// </summary>
+    public string ModuleId { get; set; }
+
+    /// <summary>
+    /// Get or set EntityTypeId
+    /// </summary>
+    public string EntityTypeId { get; set; }
+
+    /// <summary>
+    /// Get or set EntityId
+    /// </summary>
+    public string EntityId { get; set; }
+
+    /// <summary>
+    /// Get or set Field
+    /// </summary>
+    public string Field { get; set; }
+
+    /// <summary>
+    /// Get or set OldValue
+    /// </summary>
+    public string OldValue { get; set; }
+
+    /// <summary>
+    /// Get or set NewValue
+    /// </summary>
+    public string NewValue { get; set; }
+
+    /// <summary>
+    /// Get or set Comment
+    /// </summary>
+    public string Comment { get; set; }
+
+    public Guid? ParentAuditId { get; set; }
+
+}
